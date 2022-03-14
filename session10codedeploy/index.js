a51343647101896b5957d9611529de5362411b9f
emlopsinfy <90140477+emlopsinfy@users.noreply.github.com>
2022-03-14 19:31:17 +0530

    Add files via upload

---
+++ session10codedeploy/index.js
@@ -0,0 +1,16 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+const port  = 3000;
+
+app.get("/", (req, res) => {
+  res.send("CodeDeploy Sample V2!");
+});
+
+app.get("/status", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost: ${port}`);
+});
